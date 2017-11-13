8ea90ac1f0b51eeebc916e4a1f5266bb399bf7df
abdullah20 <abdullah.kf7@gmail.com>
2017-11-13 22:35:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ if(isset($_REQUEST['hub_challenge'])) {
   $challenge = $_REQUEST['hub_challenge'];
    $token = $_REQUEST['hub_verfiy_token'];
 }
-// echo "aaA";
+ echo $challenge;
 if($token == "MyCustomToken123") {
   echo $challenge;
 }
