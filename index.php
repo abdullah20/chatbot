6a9051681e1dec75f926392c248d59453b0737a7
abdullah20 <abdullah.kf7@gmail.com>
2017-11-14 23:17:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -15,12 +15,30 @@ $userID = $input['entry'][0]['messaging'][0]['sender']['id'];
 $message = $input['entry'][0]['messaging'][0]['message']['text'];
 
 $url = "https://graph.facebook.com/v2.6/me/messages?access_token=$accessToken";
+
+$i=0;
+if(!empty($input['entry'][0]['messaging'][0]['message'])){
+  i++;
+}
+  if($i==1){
+  $rep='welcome';
+  }
+    else if($i==2){
+  $rep='which city you live?';
+  }
+  
+  else if($i==3){
+  $rep='what subject you need help with';
+  }
+}
+
+   
 $jsonDate = "{
   'recipient': {
     'id': $userID
   },
   'message': {
-    'text':'$rep'
+    'text':'"$rep"'
   }
   }";
 $ch = curl_init($url);
@@ -28,7 +46,7 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDate);
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['content-Type: application/json']);
-$rep='welcome';
+
 if(!empty($input['entry'][0]['messaging'][0]['message'])){
  
   curl_exec($ch);
